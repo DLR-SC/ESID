9d077fb229b54fe6f5100690a6889925ce68c98d
SelmaDM <84981083+SelmaDM@users.noreply.github.com>
2021-07-26 09:13:20 +0200

    :tada: Add main content components.

---
+++ frontend/src/components/Scenario.tsx
@@ -0,0 +1,220 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import {Theme, createStyles, withStyles, makeStyles} from '@material-ui/core/styles';
+import {useAppDispatch} from '../store/hooks';
+import {selectScenario} from '../store/DataSelectionSlice';
+import {useAppSelector} from '../store/hooks';
+
+/* This component displays the pandemic spread depending on different scenarios
+ */
+
+const StyledTableRow = withStyles((_theme) => ({
+  root: {
+    '& .MuiTableCell-root': {
+      borderBottom: 0,
+    },
+
+    '& .MuiTable-root input': {
+      width: '100%',
+    },
+    '& .MuiTableCell-sizeSmall ': {
+      padding: '6px 28px 6px 16px',
+    },
+  },
+}))(TableRow);
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    paper: {
+      display: 'flex',
+      flexWrap: 'nowrap',
+      opacity: 0.8,
+      zIndex: 0,
+      width: '65%',
+      '& > *': {
+        margin: theme.spacing(1),
+        height: theme.spacing(28),
+        border: `3px solid`,
+        backgroundColor: '#F8F8F9',
+      },
+
+      '&>*:hover': {
+        boxShadow: '0px 0px 16px 3px ',
+      },
+
+      '& .makeStyles-paper-11 > *': {
+        margin: '6px',
+      },
+    },
+
+    table: {
+      marginTop: theme.spacing(2),
+      position: 'absolute',
+      paddingTop: '20px',
+      maxWidth: '250px',
+      opacity: 0.8,
+      backgroundColor: 'transparent',
+      zIndex: 1,
+    },
+  })
+);
+
+function createRow(
+  compartment: string,
+  latest: number,
+  basic: number,
+  basicRate: number,
+  medium: number,
+  mediumRate: number,
+  big: number,
+  bigRate: number,
+  maximum: number,
+  maximumRate: number
+) {
+  return {compartment, latest, basic, basicRate, medium, mediumRate, big, bigRate, maximum, maximumRate};
+}
+
+const header = [
+  {
+    label: '',
+    colSpan: 1,
+    color: '',
+  },
+  {
+    label: 'Now',
+    color: '#3998DB',
+    colSpan: 1,
+  },
+
+  {
+    label: ' Few contact',
+    color: '#3998DB',
+    colSpan: 2,
+  },
+  {
+    label: 'Medium contact',
+    color: '#876BE3',
+    colSpan: 2,
+  },
+  {
+    label: 'Medium contact',
+    color: '#CC5AC7',
+    colSpan: 2,
+  },
+  {
+    label: 'Strong contact',
+    color: '#EBA73B',
+    colSpan: 2,
+  },
+];
+
+const rows = [
+  createRow('infected', 100, 100, 15, 100, -50, 100, 30, 100, -50),
+  createRow('hospitalized', 145, 100, 15, 100, -50, 100, 30, 100, -50),
+  createRow('death', 160, 100, 15, 100, -50, 100, 30, 100, -50),
+  createRow('other', 170, 100, 15, 100, -50, 100, 30, 100, -50),
+];
+
+const scenario = [
+  {
+    minwidth: '160px',
+    border: 0,
+    boxShadow: '0px 0px 0px 0px ',
+  },
+  {
+    color: '#3998DB',
+    minwidth: '128px',
+  },
+  {
+    color: '#876BE3',
+    minwidth: '128px',
+  },
+  {
+    color: '#CC5AC7',
+    minwidth: '128px',
+  },
+  {
+    color: '#EBA73B',
+    minwidth: '128px',
+    border: 'solid',
+  },
+];
+
+export default function Scenario(): JSX.Element {
+  const classes = useStyles();
+  const dispatch = useAppDispatch();
+  const selectedScenario = useAppSelector((state) => state.dataSelection.scenario);
+
+  return (
+    <div>
+      <TableContainer>
+        <StyledTableRow>
+          <Table
+            className={classes.table}
+            aria-label="spanning table"
+            size="small"
+            style={{width: 'auto', tableLayout: 'auto'}}
+          >
+            <TableHead>
+              <TableRow>
+                {header.map((header, index) => (
+                  <TableCell
+                    colSpan={header.colSpan}
+                    align="center"
+                    style={{color: header.color, fontWeight: 'bold'}}
+                    key={index}
+                  >
+                    {header.label}
+                  </TableCell>
+                ))}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow
+                  hover
+                  key={row.compartment}
+                  onMouseOver={() => {
+                    dispatch(selectScenario(row.compartment));
+                  }}
+                  onMouseLeave={() => console.log(selectedScenario)}
+                >
+                  <TableCell>{row.compartment}</TableCell>
+                  <TableCell>{row.latest}</TableCell>
+                  <TableCell>{row.basic}</TableCell>
+                  <TableCell>{row.basicRate}%</TableCell>
+                  <TableCell>{row.medium}</TableCell>
+                  <TableCell>{row.mediumRate}%</TableCell>
+                  <TableCell>{row.big}</TableCell>
+                  <TableCell>{row.bigRate}%</TableCell>
+                  <TableCell>{row.maximum}</TableCell>
+                  <TableCell>{row.maximumRate}%</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </StyledTableRow>
+      </TableContainer>
+      {/* Display Cards */}
+      <div className={classes.paper}>
+        {scenario.map((scenario, index) => (
+          <Paper
+            style={{
+              color: scenario.color,
+              border: scenario.border,
+              minWidth: scenario.minwidth,
+              boxShadow: scenario.boxShadow,
+            }}
+            key={index}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
---
+++ frontend/src/components/SimulationChart.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import * as am4core from '@amcharts/amcharts4/core';
+import * as am4charts from '@amcharts/amcharts4/charts';
+import {Box} from '@material-ui/core';
+import {useEffect} from 'react';
+import {makeStyles, createStyles, Theme} from '@material-ui/core/styles';
+import am4themes_animated from '@amcharts/amcharts4/themes/animated';
+
+/* This component displays the evolution of the pandemic for a specific compartment ( Hospitalized,death,infected, etc.)
+ regarding 4 differents scenarios
+ */
+
+const useStyles = makeStyles((_theme: Theme) =>
+  createStyles({
+    chart: {
+      paddingTop: 35,
+      maxWidth: '80%',
+      height: '40vh',
+    },
+  })
+);
+
+export default function SimulationChart(): JSX.Element {
+  const classes = useStyles();
+  useEffect(() => {
+    am4core.useTheme(am4themes_animated);
+
+    // Create chart instance
+    const chart = am4core.create('chartdiv', am4charts.XYChart);
+
+    // Add data
+    chart.data = [
+      {date: new Date(2019, 5, 12), value1: 50, value2: 48, value3: 45, value4: 40, previousDate: new Date(2019, 5, 5)},
+      {date: new Date(2019, 5, 13), value1: 53, value2: 51, value3: 48, value4: 46, previousDate: new Date(2019, 5, 6)},
+      {date: new Date(2019, 5, 14), value1: 56, value2: 57, value3: 55, value4: 50, previousDate: new Date(2019, 5, 7)},
+      {date: new Date(2019, 5, 15), value1: 52, value2: 53, value3: 50, value4: 48, previousDate: new Date(2019, 5, 8)},
+      {date: new Date(2019, 5, 16), value1: 48, value2: 44, value3: 42, value4: 40, previousDate: new Date(2019, 5, 9)},
+      {
+        date: new Date(2019, 5, 17),
+        value1: 47,
+        value2: 42,
+        value3: 40,
+        value4: 38,
+        previousDate: new Date(2019, 5, 10),
+      },
+      {
+        date: new Date(2019, 5, 18),
+        value1: 59,
+        value2: 55,
+        value3: 50,
+        value4: 48,
+        previousDate: new Date(2019, 5, 11),
+      },
+    ];
+
+    // Create axes
+    const dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.logarithmic = true;
+    dateAxis.renderer.minGridDistance = 50;
+    chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Create series
+    const series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = 'value1';
+    series.dataFields.dateX = 'date';
+    series.tensionX = 0.8;
+    series.strokeWidth = 1;
+    series.tensionX = 0.8;
+    series.tooltipText =
+      '{date.formatDate()}:[/] {value1}\n {previousDate.formatDate()}:[/] {value2}\n{date.formatDate()}:[/] {value3}\n{date.formatDate()}:[/] {value4}';
+    series.fill = am4core.color('#3998DB');
+    series.stroke = am4core.color('#3998DB');
+    // Create series
+    const series2 = chart.series.push(new am4charts.LineSeries());
+    series2.dataFields.valueY = 'value2';
+    series2.dataFields.dateX = 'date';
+    series2.tensionX = 0.8;
+    series.fill = am4core.color('#876BE3');
+    series.stroke = am4core.color('#876BE3');
+    series2.strokeWidth = 1;
+    series2.stroke = series.stroke;
+    // Create series
+    const series3 = chart.series.push(new am4charts.LineSeries());
+    series3.dataFields.valueY = 'value3';
+    series3.dataFields.dateX = 'date';
+    series3.tensionX = 0.8;
+    series3.strokeWidth = 1;
+    series.fill = am4core.color('#CC5AC7');
+    series.stroke = am4core.color('#CC5AC7');
+    // Create series
+    const series4 = chart.series.push(new am4charts.LineSeries());
+    series4.dataFields.valueY = 'value4';
+    series4.dataFields.dateX = 'date';
+    series.tensionX = 0.8;
+    series4.strokeWidth = 1;
+    series4.stroke = series.stroke;
+    series.fill = am4core.color('#EBA73B');
+    series.stroke = am4core.color('#EBA73B');
+
+    // Add cursor
+    chart.cursor = new am4charts.XYCursor();
+    chart.cursor.xAxis = dateAxis;
+  }, []);
+
+  return (
+    <Box id="chartdiv" className={classes.chart}>
+      {' '}
+    </Box>
+  );
+}
