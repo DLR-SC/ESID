294acfefb24e3ceb8d2cbb4742bd6725901c785b
SelmaDM <84981083+SelmaDM@users.noreply.github.com>
2021-07-30 14:07:26 +0200

    :wrench: Render the map only once.

--- frontend/src/components/MapCountry.tsx
+++ frontend/src/components/MapCountry.tsx
@@ -8,6 +8,7 @@ import {useTranslation} from 'react-i18next';
 import {useAppDispatch} from '../store/hooks';
 import {selectDistrict} from '../store/DataSelectionSlice';
 import {useAppSelector} from '../store/hooks';
+const {useRef} = React;
 
 const useStyles = makeStyles({
   Map: {
@@ -43,65 +44,37 @@ interface IRegionPolygon {
  * The colors depends on temporary values assigned to each region.
  */
 export default function MapCountry(): JSX.Element {
+  const selectedScenario = useAppSelector((state) => state.dataSelection.scenario);
+  const chartRef = useRef<am4maps.MapChart | null>(null);
+
   const {t} = useTranslation('global');
   const classes = useStyles();
-  const selectedScenario = useAppSelector((state) => state.dataSelection.scenario);
   const dispatch = useAppDispatch();
 
+  //Chart
   useEffect(() => {
-    let regionPolygon: IRegionPolygon;
-
-    // Colors set of the Map
-    const heatColors1 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
-    const heatColors2 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
-    const heatColors3 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
-
     // Create map instance
     const chart = am4core.create('chartdiv', am4maps.MapChart);
-
     // Set map definition
     chart.geodataSource.url = 'assets/lk_germany_reduced.geojson';
-
     // Set projection
     chart.projection = new am4maps.projections.Mercator();
+    // Zoom control
+    chart.zoomControl = new am4maps.ZoomControl();
+    chart.zoomControl.align = 'left';
+    chart.zoomControl.paddingBottom = 25;
+    chart.zoomControl.opacity = 50;
+    chart.seriesContainer.draggable = true;
+    dispatch(selectDistrict({ags: '00000', name: t('germany'), type: ''}));
 
-    //Create map polygon series
-    const polygonSeries = chart.series.push(new am4maps.MapPolygonSeries());
+    chartRef.current = chart;
+    return () => {
+      chartRef.current && chartRef.current.dispose();
+    };
+  }, [t, dispatch]);
 
-    // Configure series
-    polygonSeries.mapPolygons.template.tooltipPosition = 'fixed';
-    const polygonTemplate = polygonSeries.mapPolygons.template;
-
-    polygonTemplate.events.on('hit', (e) => {
-      const item = e.target.dataItem.dataContext as IRegionPolygon;
-      dispatch(selectDistrict({ags: item.RS, name: item.GEN, type: t(item.BEZ)}));
-    });
-
-    //Set values to each regions
-    polygonSeries.events.on('validated', (event) => {
-      event.target.mapPolygons.each((mapPolygon) => {
-        regionPolygon = mapPolygon.dataItem.dataContext as IRegionPolygon;
-        regionPolygon.value = Math.floor(Math.random() * 300);
-        // add tooltipText
-        mapPolygon.tooltipText = t(`BEZ.${regionPolygon.BEZ}`) + ' {GEN}' + '  ' + selectedScenario;
-      });
-    });
-
-    //Assign colors to regions
-    polygonSeries.events.on('validated', (event) => {
-      event.target.mapPolygons.each((mapPolygon) => {
-        regionPolygon = mapPolygon.dataItem.dataContext as IRegionPolygon;
-        const colorIndex = Math.floor(Math.random() * 2);
-        if (regionPolygon.value <= 100) {
-          mapPolygon.fill = heatColors1[colorIndex];
-        } else if (regionPolygon.value >= 200 && regionPolygon.value <= 270) {
-          mapPolygon.fill = heatColors2[colorIndex];
-        } else {
-          mapPolygon.fill = heatColors3[colorIndex];
-        }
-      });
-    });
-
+  // Heat Legend
+  useEffect(() => {
     // add heat legend container
     const legendContainer = am4core.create('legenddiv', am4core.Container);
     legendContainer.width = am4core.percent(100);
@@ -112,21 +85,59 @@ export default function MapCountry(): JSX.Element {
     heatLegend.minColor = am4core.color('#F8F8F9');
     heatLegend.maxColor = am4core.color('#F8F8F9');
     heatLegend.align = 'center';
+  }, []);
 
-    // Zoom control
-    chart.zoomControl = new am4maps.ZoomControl();
-    chart.zoomControl.align = 'left';
-    chart.zoomControl.paddingBottom = 25;
-    chart.zoomControl.opacity = 50;
-    chart.seriesContainer.draggable = true;
-    polygonSeries.useGeodata = true;
 
-    // Create hover state and set alternative fill color
-    const hs = polygonTemplate.states.create('hover');
-    hs.properties.fill = am4core.color('#367B25');
+  //Polygone
+  useEffect(() => {
+    let regionPolygon: IRegionPolygon;
+    // Colors set of the Map
+    const heatColors1 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
+    const heatColors2 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
+    const heatColors3 = [am4core.color('#34BEC7'), am4core.color('#3998DB'), am4core.color('#3abedf')];
 
-    dispatch(selectDistrict({ags: '00000', name: t('germany'), type: ''}));
-  }, [t, dispatch, selectedScenario]);
+    if (chartRef.current) {
+      //Create map polygon series
+      const polygonSeries = chartRef.current.series.push(new am4maps.MapPolygonSeries());
+      // Configure series
+      polygonSeries.mapPolygons.template.tooltipPosition = 'fixed';
+      const polygonTemplate = polygonSeries.mapPolygons.template;
+      polygonTemplate.events.on('hit', (e) => {
+        const item = e.target.dataItem.dataContext as IRegionPolygon;
+        dispatch(selectDistrict({ags: item.RS, name: item.GEN, type: t(item.BEZ)}));
+      });
+
+      //Set values to each regions
+      polygonSeries.events.on('validated', (event) => {
+        event.target.mapPolygons.each((mapPolygon) => {
+          regionPolygon = mapPolygon.dataItem.dataContext as IRegionPolygon;
+          regionPolygon.value = Math.floor(Math.random() * 300);
+          // add tooltipText
+          mapPolygon.tooltipText = t(`BEZ.${regionPolygon.BEZ}`) + ' {GEN}' + '  ' + selectedScenario;
+        });
+      });
+
+      //Assign colors to regions
+      polygonSeries.events.on('validated', (event) => {
+        event.target.mapPolygons.each((mapPolygon) => {
+          regionPolygon = mapPolygon.dataItem.dataContext as IRegionPolygon;
+          const colorIndex = Math.floor(Math.random() * 2);
+          if (regionPolygon.value <= 100) {
+            mapPolygon.fill = heatColors1[colorIndex];
+          } else if (regionPolygon.value >= 200 && regionPolygon.value <= 270) {
+            mapPolygon.fill = heatColors2[colorIndex];
+          } else {
+            mapPolygon.fill = heatColors3[colorIndex];
+          }
+        });
+      });
+
+      polygonSeries.useGeodata = true;
+      // Create hover state and set alternative fill color
+      const hs = polygonTemplate.states.create('hover');
+      hs.properties.fill = am4core.color('#367B25');
+    }
+  }, [selectedScenario]);
 
   return (
     <>
