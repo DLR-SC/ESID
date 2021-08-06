9750c9cd6ade2a9a06bd3bf815dfd9c903d90980
SelmaDM <84981083+SelmaDM@users.noreply.github.com>
2021-08-06 10:23:38 +0200

    :art: Add divider .

--- frontend/src/components/MainContent.tsx
+++ frontend/src/components/MainContent.tsx
@@ -2,6 +2,7 @@ import {Grid, makeStyles} from '@material-ui/core';
 import React from 'react';
 import Scenario from './Scenario';
 import Icones from './Icones';
+import Divider from '@material-ui/core/Divider';
 
 import SimulationChart from './SimulationChart';
 
@@ -10,19 +11,27 @@ const useStyles = makeStyles({
     marginLeft: 'auto',
     marginRight: 'auto',
   },
+  divider: {
+    borderBottom: '2px dotted #3998DB',
+    backgroundColor: 'transparent',
+    margin: '20px 0 0 0',
+  },
 });
 
 export default function MainContent(): JSX.Element {
   const classes = useStyles();
   return (
-    <Grid container direction="column" wrap="nowrap">
+    <Grid container direction="column" style={{width: '100vw'}}>
       <Grid item className={classes.icon}>
         <Icones />
       </Grid>
-      <Grid item>
+      <Grid item style={{width: '100%'}}>
         <Scenario />
       </Grid>
-      <Grid item style={{flexGrow: 1}}>
+      <Grid item style={{width: '100%'}}>
+        <Divider className={classes.divider} light />
+      </Grid>
+      <Grid item style={{width: '100%', flexGrow: 1}}>
         <SimulationChart />
       </Grid>
     </Grid>
