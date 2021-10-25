dd1b623d2e7bc8e7ee2f7b2d10b4390310fa84ed
Jonas Gilg <jonas.gilg@dlr.de>
2021-10-25 15:40:39 +0200

    :wrench: Simplify the icon bar.

--- frontend/src/components/IconBar.tsx
+++ frontend/src/components/IconBar.tsx
@@ -5,22 +5,9 @@ import UndoIcon from '@mui/icons-material/Undo';
 import FullscreenIcon from '@mui/icons-material/Fullscreen';
 import ShareIcon from '@mui/icons-material/Share';
 import {useFullscreen} from 'rooks';
-import {createStyles, makeStyles} from '@mui/styles';
-import {Theme} from '@mui/material/styles';
 import {Box, Button} from '@mui/material';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      '& > svg': {
-        margin: theme.spacing(2),
-      },
-    },
-  })
-);
-
 export default function IconBar(): JSX.Element {
-  const classes = useStyles();
   const fsApi = useFullscreen();
 
   const toggleFullscreen = () => {
@@ -32,24 +19,25 @@ export default function IconBar(): JSX.Element {
   };
 
   return (
-    <Box display='flex' height={60}>
-      <Box className={classes.root} m='auto'>
-        <Button disabled>
-          <AutorenewIcon />
-        </Button>
-        <Button disabled>
-          <UndoIcon />
-        </Button>
-        <Button disabled>
-          <RedoIcon />
-        </Button>
-        <Button>
-          <FullscreenIcon onClick={toggleFullscreen} />
-        </Button>
-        <Button disabled>
-          <ShareIcon />
-        </Button>
-      </Box>
+    <Box
+      sx={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}
+      height={60}
+    >
+      <Button disabled>
+        <AutorenewIcon />
+      </Button>
+      <Button disabled>
+        <UndoIcon />
+      </Button>
+      <Button disabled>
+        <RedoIcon />
+      </Button>
+      <Button>
+        <FullscreenIcon onClick={toggleFullscreen} />
+      </Button>
+      <Button disabled>
+        <ShareIcon />
+      </Button>
     </Box>
   );
 }
