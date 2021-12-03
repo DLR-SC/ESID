3c2927434cd8398056f75b28acf873d8f5398a40
Luca Spataro <luca@spataro.de>
2021-12-03 16:01:52 +0100

    add admin command for importing and deleting a scenario

---
+++ backend/src/api/management/commands/import_scenario.py
@@ -0,0 +1,97 @@
+from django.core.management.base import BaseCommand, CommandError
+from datetime import datetime
+from tqdm import tqdm
+import pandas as pd
+import src.api.models as models
+import json 
+
+MANDATORY = [
+    "name",
+    "numberOfGroups",
+    "groups",
+    "numberOfNodes",
+    "nodes",
+    "simulationModel",
+    "parameters"
+]
+
+class Command(BaseCommand):
+    help = 'Import a new scenario'
+
+    def add_arguments(self, parser):
+        parser.add_argument('config', type=str)
+
+    def handle(self, *args, **options):
+        self.stdout.write('Importing scenario from file "{}"'.format(options['config']))
+
+        with open(options['config']) as file:
+            config = json.load(file)
+        
+        # check mandatory keys
+        for key in MANDATORY:
+            if key not in config:
+                raise CommandError('Mandatory key "{}" is missing!'.format(key))
+
+        
+        scenario = models.Scenario()
+        scenario.name = config['name']
+        scenario.description = config.get('description', '')
+        scenario.number_of_groups = config['numberOfGroups']
+        scenario.number_of_nodes = config['numberOfNodes']
+        
+        # select simulation model
+        try:
+            simulation_model = models.SimulationModel.objects.get(name=config['simulationModel'])
+        except models.SimulationModel.DoesNotExist:
+            raise CommandError('Simulation model "{}" does not exist'.format(config['simulationModel']))
+
+        scenario.simulation_model = simulation_model
+
+        scenario.save()
+
+        # select group models and create if neccessary
+        groups = []
+        for group_info in tqdm(config['groups'], total=len(config['groups']), desc="Creating groups"):
+            [group, created] = models.Group.objects.get_or_create(name=group_info['name'])
+            groups.append(group)
+
+        nodes = config['nodes']
+        parameters = list(simulation_model.parameters.values_list('name', flat=True))
+
+        # create scenario nodes
+        for node_key in tqdm(nodes, total=len(nodes), desc="Creating scenario nodes"):
+            # select reference node
+            try:
+                node = models.Node.objects.get(metadata__key=node_key)
+            except models.Node.DoesNotExist:
+                raise CommandError('Node "{}" does not exist'.format(node_key))
+
+            scenario_node = models.ScenarioNode()
+            scenario_node.node = node
+            scenario_node.save()
+            
+            # create parameters for node
+            with tqdm(total=len(groups) * len(parameters), leave=False, desc="Creating node parameters") as progress:
+                for parameter_name in parameters:
+                    if parameter_name not in config['parameters']:
+                        raise CommandError('Values for parameter "{}" are missing '.format(parameter_name))
+
+                    parameter_info = config['parameters'][parameter_name]
+                    parameter = models.Parameter.objects.get(name=parameter_name)
+                    for i, group in enumerate(groups):
+                        # if group specific values exist use them, otherwise use global values
+                        [min_value, max_value] = parameter_info['groups'][i] if 'groups' in parameter_info else parameter_info['value']
+                        [distribution, created] = models.Distribution.objects.get_or_create(min=min_value, max=max_value, type='Normal', value=0.0)
+                        
+                        scenario_parameter = models.ScenarioParameter(distribution=distribution, group=group, parameter=parameter)
+                        scenario_parameter.save()
+                        
+                        scenario_node.parameters.add(scenario_parameter)
+                        progress.update()
+
+            scenario_node.save()
+            scenario.nodes.add(scenario_node)
+
+        scenario.save()
+        self.stdout.write(self.style.SUCCESS('Successfully imported scenario "{}"'.format(scenario.name)))
+
