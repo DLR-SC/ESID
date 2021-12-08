0ea047b54c35c427b18271f695408089199fbbdb
Mo <25636783+NXXR@users.noreply.github.com>
2021-12-08 11:32:54 +0100

    Revert ":sparkles: run formatter"
    This reverts commit b65ad78d78875fb656e16fff7b78721b650a46e2.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -48,7 +48,11 @@ const theme = createTheme({
     divider: '#d2d1db',
   },
   typography: {
-    fontFamily: ['Inter', 'Arial', 'sans-serif'].join(','),
+    fontFamily: [
+      'Inter',
+      'Arial',
+      'sans-serif',
+    ].join(','),
     fontSize: 13,
     h1: {
       fontSize: 16,
