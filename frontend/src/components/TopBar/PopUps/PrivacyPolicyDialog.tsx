aab3206f556bb3f7302435759dd7f4427302ef33
Mo <25636783+NXXR@users.noreply.github.com>
2021-12-16 15:28:17 +0100

    :wrench: Add theming to privacyPolicyDialog Component

--- frontend/src/components/TopBar/PopUps/PrivacyPolicyDialog.tsx
+++ frontend/src/components/TopBar/PopUps/PrivacyPolicyDialog.tsx
@@ -1,28 +1,27 @@
 import {Typography} from '@mui/material';
-import {makeStyles} from '@mui/styles';
+import {useTheme} from '@mui/material/styles';
 import React from 'react';
 
 import {useTranslation} from 'react-i18next';
-
-const useStyles = makeStyles({
-  dialogStyle: {
-    padding: '20px',
-    background: '#f8f8f8',
-  },
-});
+import {Box} from '@mui/system';
 
 /**
  * This component displays the privacy policy legal text.
  */
 export default function PrivacyPolicyDialog(): JSX.Element {
   const {t} = useTranslation('legal');
-  const classes = useStyles();
+  const theme = useTheme();
 
   return (
-    <div className={classes.dialogStyle}>
-      <Typography variant='h3'>{t('privacy-policy.header')}</Typography>
+    <Box
+      sx={{
+        padding: theme.spacing(4),
+        background: theme.palette.background.paper,
+      }}
+    >
+      <Typography variant='h1'>{t('privacy-policy.header')}</Typography>
       {/* While it says that it is dangerous, it is fine here. Only static content is inserted. */}
       <div dangerouslySetInnerHTML={{__html: t('privacy-policy.content')}} />
-    </div>
+    </Box>
   );
 }
