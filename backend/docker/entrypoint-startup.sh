9ba7fe3fb5855213e5b25d0fd90bfa31d0cbde4f
Luca Spataro <luca@spataro.de>
2022-01-18 14:21:06 +0100

    Update docker build to only copy what is needed

---
+++ backend/docker/entrypoint-startup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+set -e
+
+python manage.py migrate
+python manage.py collectstatic --noinput
+
+gunicorn --bind 0.0.0.0:8000 -w 4 --limit-request-line 6094 --access-logfile - src.wsgi:application
