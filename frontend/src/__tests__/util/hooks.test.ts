40d004977894d334d7d360e5d4e6cccf38d31297
Jonas Gilg <jonas.gilg@dlr.de>
2022-01-21 09:55:53 +0100

    :wrench: Add tests for the NumberFormatter.

--- frontend/src/__tests__/util/hooks.test.ts
+++ frontend/src/__tests__/util/hooks.test.ts
@@ -15,4 +15,30 @@ describe('NumberFormatter', () => {
     const englishFormat = result.current.formatNumber(2000.1);
     expect(englishFormat).toBe('2,000.1');
   });
+
+  test('significantDigits', () => {
+    const {result} = renderHook(() => NumberFormatter('en', 5, 5));
+    const formatNumber = result.current.formatNumber;
+
+    expect(formatNumber(123456)).toBe('123,456');
+    expect(formatNumber(12345.6)).toBe('12,346');
+    expect(formatNumber(1234.56)).toBe('1,234.6');
+    expect(formatNumber(123.456)).toBe('123.46');
+    expect(formatNumber(12.3456)).toBe('12.346');
+    expect(formatNumber(1.23456)).toBe('1.2346');
+    expect(formatNumber(0.123456)).toBe('0.12346');
+  });
+
+  test('maxFractionalDigits', () => {
+    const {result} = renderHook(() => NumberFormatter('en', 5, 2));
+    const formatNumber = result.current.formatNumber;
+
+    expect(formatNumber(123456)).toBe('123,456');
+    expect(formatNumber(12345.6)).toBe('12,346');
+    expect(formatNumber(1234.56)).toBe('1,234.6');
+    expect(formatNumber(123.456)).toBe('123.46');
+    expect(formatNumber(12.3456)).toBe('12.35');
+    expect(formatNumber(1.23456)).toBe('1.23');
+    expect(formatNumber(0.123456)).toBe('0.12');
+  });
 });
