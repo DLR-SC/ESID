cbb036011b14d38aa17ad17e883f8b675a0ab3a2
kerklohlaurin <laurin.kerkloh@dlr.de>
2022-04-04 13:49:30 +0200

    :heavy_check_mark: Add tests for UserPreferenceSlice

---
+++ frontend/src/__tests__/store/UserPreferenceSlice.test.ts
@@ -0,0 +1,68 @@
+import reducer, {selectHeatmapLegend, setHeatmapLegends, UserPreference} from "../../store/UserPreferenceSlice";
+import {HeatmapLegend} from "../../types/heatmapLegend";
+
+describe('DataSelectionSlice', () => {
+  const initialState: UserPreference = {
+    selectedHeatmap: {
+      name: "error",
+      isNormalized: true,
+      steps: [
+        {color: "rgb(255,255,255)", value: 0},
+        {color: "rgb(255,255,255)", value: 1}
+      ]
+    },
+    heatmaps: []
+  }
+
+  test('Initial State', () => {
+    expect(reducer(undefined, {type: null})).toEqual(initialState);
+  });
+
+  test('Select Heatmap Legend', () => {
+    const legend: HeatmapLegend = {
+      name: "test",
+      isNormalized: true,
+      steps: [
+        {color: "rgb(255,255,255)", value: 0},
+        {color: "rgb(204,11,234)", value: 1}
+      ]
+    };
+    expect(reducer(initialState, selectHeatmapLegend({legend: legend}))).toEqual({
+      selectedHeatmap: legend,
+      heatmaps: []
+    });
+  });
+
+  test('Set Heatmap Legends', () => {
+    const legends: HeatmapLegend[] = [{
+      name: "test 1",
+      isNormalized: true,
+      steps: [
+        {color: "rgb(255,255,255)", value: 0},
+        {color: "rgb(204,11,234)", value: 1}
+      ]
+    },
+      {
+        name: "test 2",
+        isNormalized: false,
+        steps: [
+          {color: "rgb(94,254,76)", value: 0},
+          {color: "rgb(225,199,18)", value: 150},
+          {color: "rgb(194,96,14)", value: 500},
+          {color: "rgb(164,25,42)", value: 2000}
+        ]
+      }]
+
+    expect(reducer(initialState, setHeatmapLegends({legends: legends}))).toEqual({
+      selectedHeatmap: {
+        name: "error",
+        isNormalized: true,
+        steps: [
+          {color: "rgb(255,255,255)", value: 0},
+          {color: "rgb(255,255,255)", value: 1}
+        ]
+      },
+      heatmaps: legends
+    })
+  });
+})
