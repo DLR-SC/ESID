64b813f77baaf87956096f69f2eaa43ced4e3887
kerklohlaurin <laurin.kerkloh@dlr.de>
2022-04-11 14:44:25 +0200

    :tada: Default Heatmap Legend is now in the Scenario Colors

--- frontend/src/components/Sidebar/HeatLegendEdit.tsx
+++ frontend/src/components/Sidebar/HeatLegendEdit.tsx
@@ -1,17 +1,23 @@
-import React, {useEffect} from 'react';
-import {useTheme} from '@mui/material/styles';
-import {Box, Dialog, Grid, IconButton, Radio, Typography} from '@mui/material';
-import HeatLegend from './HeatLegend';
-import EditIcon from '@mui/icons-material/Edit';
-import {useAppDispatch, useAppSelector} from '../../store/hooks';
-import {selectHeatmapLegend, setHeatmapLegends} from '../../store/UserPreferenceSlice';
-import {HeatmapLegend} from '../../types/heatmapLegend';
+import React, { useEffect } from "react";
+import { useTheme } from "@mui/material/styles";
+import { Box, Dialog, Grid, IconButton, Radio, Typography } from "@mui/material";
+import HeatLegend from "./HeatLegend";
+import EditIcon from "@mui/icons-material/Edit";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import {
+  selectDefaultLegend,
+  selectHeatmapLegend,
+  setDefaultLegends,
+  setHeatmapLegends
+} from "../../store/UserPreferenceSlice";
+import { HeatmapLegend } from "../../types/heatmapLegend";
 
 /**
  * This component displays an edit button to access a modal. In the modal you can edit the heatmap legend.
  */
 export default function HeatLegendEdit(): JSX.Element {
   const dispatch = useAppDispatch();
+  const activeScenario = useAppSelector((state) => state.dataSelection.scenario);
   const legend = useAppSelector((state) => state.userPreference.selectedHeatmap);
   const presets = useAppSelector((state) => state.userPreference.heatmaps);
   const theme = useTheme();
@@ -21,16 +27,16 @@ export default function HeatLegendEdit(): JSX.Element {
     setHeatLegendEditOpen(true);
   };
   const handleClick = (preset: HeatmapLegend) => {
-    dispatch(selectHeatmapLegend({legend: preset}));
+    dispatch(selectHeatmapLegend({ legend: preset }));
   };
 
   useEffect(() => {
     // get heatmap legend preset list from assets and select default
-    fetch('assets/heatmap_legend_presets.json', {
+    fetch("assets/heatmap_legend_presets.json", {
       headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      }
     })
       .then((response) => {
         // interpret content as JSON
@@ -45,41 +51,60 @@ export default function HeatLegendEdit(): JSX.Element {
                 step.value = step.value / legend.steps[legend.steps.length - 1].value;
               });
             }
-            if (legend.name == 'Default') {
-              dispatch(selectHeatmapLegend({legend: legend}));
-            }
           });
+          //create default legends
+          const defaultLegends: HeatmapLegend[] = [];
+          const stepCount = theme.custom.scenarios[0].length - 1;
+          for (let i = 0; i < theme.custom.scenarios.length; i++) {
+            const steps = [];
+            for (let j = 0; j < stepCount; j++) {
+              steps.push({ color: theme.custom.scenarios[i][stepCount-1 - j], value: j / (stepCount - 1) });
+            }
+            defaultLegends.push({ name: "Default", isNormalized: true, steps });
+          }
+          dispatch(setDefaultLegends({legends: defaultLegends}));
+
+          //add default preset to the front of the presetList
+          presetList.unshift(defaultLegends[0]);
           // fill presets state with list
-          dispatch(setHeatmapLegends({legends: presetList}));
+          dispatch(setHeatmapLegends({ legends: presetList }));
+          dispatch(selectHeatmapLegend({ legend: defaultLegends[0] }));
         },
         // Reject Promise
         () => {
-          console.warn('Did not receive proper heatmap legend presets');
+          console.warn("Did not receive proper heatmap legend presets");
         }
       );
     // this init should only run once on first render
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  useEffect(() => {
+    if (activeScenario) {
+      dispatch(selectDefaultLegend({ selectedScenario: activeScenario - 1 }))
+    }
+
+  }, [activeScenario, dispatch]);
+
   return (
     <>
       <IconButton onClick={heatLegendEditClicked}>
         <EditIcon />
       </IconButton>
-      <Dialog maxWidth='lg' fullWidth={true} open={heatLegendEditOpen} onClose={() => setHeatLegendEditOpen(false)}>
+      <Dialog maxWidth="lg" fullWidth={true} open={heatLegendEditOpen} onClose={() => setHeatLegendEditOpen(false)}>
         <Box
           sx={{
             padding: theme.spacing(4),
-            background: theme.palette.background.paper,
+            background: theme.palette.background.paper
           }}
         >
           <Grid container>
             {presets.map((preset, i) => (
-              <Grid item container xs={12} md={6} key={'legendPreset' + i.toString()}>
-                <Grid item container xs={1} alignItems='center' justifyContent='flex-end'>
+              <Grid item container xs={12} md={6} key={"legendPreset" + i.toString()}>
+                <Grid item container xs={1} alignItems="center" justifyContent="flex-end">
                   <Radio
                     checked={preset.name === legend.name}
-                    name='radio-buttons'
+                    name="radio-buttons"
                     value={preset.name}
                     onChange={() => handleClick(preset)}
                   />
@@ -95,7 +120,7 @@ export default function HeatLegendEdit(): JSX.Element {
                     noText={preset.isNormalized}
                     id={preset.name}
                   />
-                  <Typography variant='h2' align='center'>
+                  <Typography variant="h2" align="center">
                     {preset.name}
                   </Typography>
                 </Grid>
--- frontend/src/store/UserPreferenceSlice.ts
+++ frontend/src/store/UserPreferenceSlice.ts
@@ -1,38 +1,50 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit';
-import {HeatmapLegend} from '../types/heatmapLegend';
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { HeatmapLegend } from "../types/heatmapLegend";
 
 export interface UserPreference {
   selectedHeatmap: HeatmapLegend;
   heatmaps: HeatmapLegend[];
+  defaultHeatmaps: HeatmapLegend[];
 }
 
 const initialState: UserPreference = {
+  //Heatmaps are initialized in the HeatLegendEdit Component
   selectedHeatmap: {
-    name: 'error',
+    name: "error",
     isNormalized: true,
     steps: [
-      {color: 'rgb(255,255,255)', value: 0},
-      {color: 'rgb(255,255,255)', value: 1},
-    ],
+      { color: "rgb(255,255,255)", value: 0 },
+      { color: "rgb(255,255,255)", value: 1 }
+    ]
   },
   heatmaps: [],
+  defaultHeatmaps: []
 };
 
 /**
  * This slice manages all state that has to do with user preferences.
  */
 export const UserPreferenceSlice = createSlice({
-  name: 'UserPreference',
+  name: "UserPreference",
   initialState,
   reducers: {
-    selectHeatmapLegend(state, action: PayloadAction<{legend: HeatmapLegend}>) {
+    selectHeatmapLegend(state, action: PayloadAction<{ legend: HeatmapLegend }>) {
       state.selectedHeatmap = action.payload.legend;
     },
-    setHeatmapLegends(state, action: PayloadAction<{legends: HeatmapLegend[]}>) {
+    setHeatmapLegends(state, action: PayloadAction<{ legends: HeatmapLegend[] }>) {
       state.heatmaps = action.payload.legends;
     },
-  },
+    selectDefaultLegend(state, action: PayloadAction<{ selectedScenario: number }>) {
+      state.heatmaps[0] = state.defaultHeatmaps[action.payload.selectedScenario];
+      if (state.selectedHeatmap.name == "Default") {
+        state.selectedHeatmap = state.heatmaps[0];
+      }
+    },
+    setDefaultLegends(state, action:PayloadAction<{legends: HeatmapLegend[]}>){
+      state.defaultHeatmaps = action.payload.legends;
+    }
+  }
 });
 
-export const {selectHeatmapLegend, setHeatmapLegends} = UserPreferenceSlice.actions;
+export const { selectHeatmapLegend, setHeatmapLegends, selectDefaultLegend, setDefaultLegends } = UserPreferenceSlice.actions;
 export default UserPreferenceSlice.reducer;
