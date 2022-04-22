17a9b4a158278b28739a94a1b9d48dcb0c19bd24
kerklohlaurin <laurin.kerkloh@dlr.de>
2022-04-22 13:05:14 +0200

    :memo: adds some comments

--- frontend/src/store/UserPreferenceSlice.ts
+++ frontend/src/store/UserPreferenceSlice.ts
@@ -28,18 +28,22 @@ export const UserPreferenceSlice = createSlice({
   name: 'UserPreference',
   initialState,
   reducers: {
+    //set currently selected HeatmapLegend
     selectHeatmapLegend(state, action: PayloadAction<{legend: HeatmapLegend}>) {
       state.selectedHeatmap = action.payload.legend;
     },
+    //set List off all HeatmapLegends
     setHeatmapLegends(state, action: PayloadAction<{legends: HeatmapLegend[]}>) {
       state.heatmaps = action.payload.legends;
     },
+    //select the right colored (according to selected Scenario) default HeatmapLegend
     selectDefaultLegend(state, action: PayloadAction<{selectedScenario: number}>) {
       state.heatmaps[0] = state.defaultHeatmaps[action.payload.selectedScenario];
       if (state.selectedHeatmap.name == 'Default') {
         state.selectedHeatmap = state.heatmaps[0];
       }
     },
+    //set List of all default HeatmapLegend
     setDefaultLegends(state, action: PayloadAction<{legends: HeatmapLegend[]}>) {
       state.defaultHeatmaps = action.payload.legends;
     },
