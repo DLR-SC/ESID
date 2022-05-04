50924ca60e2842bb905edc4c9b700d1db0c03bfa
Luca Spataro <luca@spataro.de>
2022-05-04 09:59:49 +0200

    Allow multiple groups for scenario parameter and adapt import scripts

--- backend/src/api/management/commands/import_scenario.py
+++ backend/src/api/management/commands/import_scenario.py
@@ -5,7 +5,6 @@ import json
 
 MANDATORY = [
     "name",
-    "numberOfGroups",
     "groups",
     "numberOfNodes",
     "nodes",
@@ -32,15 +31,15 @@ class Command(BaseCommand):
 
         
         scenario = models.Scenario()
-        scenario.key = config['name']
+        scenario.key = config['key']
         scenario.name = config['name']
         scenario.description = config.get('description', '')
-        scenario.number_of_groups = config['numberOfGroups']
+        scenario.number_of_groups = len(config['groups'])
         scenario.number_of_nodes = config['numberOfNodes']
         
         # select simulation model
         try:
-            simulation_model = models.SimulationModel.objects.get(name=config['simulationModel'])
+            simulation_model = models.SimulationModel.objects.get(key=config['simulationModel'])
         except models.SimulationModel.DoesNotExist:
             raise CommandError('Simulation model "{}" does not exist'.format(config['simulationModel']))
 
@@ -49,10 +48,18 @@ class Command(BaseCommand):
         scenario.save()
 
         # select group models and create if neccessary
-        groups = []
+        all_groups = []
         for group_info in tqdm(config['groups'], total=len(config['groups']), desc="Creating groups"):
-            [group, created] = models.Group.objects.get_or_create(key=group_info['name'])
-            groups.append(group)
+            group = None
+
+            try:
+                group = models.Group.objects.get(key=group_info['key'])
+            except models.Group.DoesNotExist:
+                if all(key in group_info for key in ['key', 'name', 'description', 'category']):
+                    group = models.Group.objects.create(key=group_info['key'], category=models.GroupCategory.objects.get(key=group_info['category']), name=group_info['name'], description=group_info['description'])
+            
+            if group is not None:
+                all_groups.append(group)
 
         nodes = config['nodes']
         parameters = list(simulation_model.parameters.values_list('name', flat=True))
@@ -70,27 +77,42 @@ class Command(BaseCommand):
             scenario_node.save()
             
             # create parameters for node
-            with tqdm(total=len(groups) * len(parameters), leave=False, desc="Creating node parameters") as progress:
+            with tqdm(total=len(parameters), leave=False, desc="Creating node parameters") as progress:
                 for parameter_name in parameters:
                     if parameter_name not in config['parameters']:
                         raise CommandError('Values for parameter "{}" are missing '.format(parameter_name))
 
                     parameter_info = config['parameters'][parameter_name]
                     parameter = models.Parameter.objects.get(name=parameter_name)
-                    scenario_parameter = models.ScenarioParameter(parameter=parameter)
-                    scenario_parameter.save()
+                    scenario_parameter = models.ScenarioParameter.objects.create(parameter=parameter)
 
                     group_parameters = []
 
-                    for i, group in enumerate(groups):
+                    for i, parameter_group in enumerate(parameter_info):
                         # if group specific values exist use them, otherwise use global values
-                        [min_value, max_value] = parameter_info['groups'][i] if 'groups' in parameter_info else parameter_info['value']
-                        [distribution, created] = models.Distribution.objects.get_or_create(min=min_value, max=max_value, type='Normal', value=0.0)
-                        group_parameter = models.ScenarioParameterGroup(group=group, distribution=distribution)
-                        group_parameters.append(group_parameter)
-                        progress.update()
+                        [min_value, max_value] = parameter_group['value']
 
-                    models.ScenarioParameterGroup.objects.bulk_create(group_parameters)
+                        if 'category' in parameter_group:
+                            # select all groups for given category
+                            groups = list(models.Group.objects \
+                                        .filter(category=models.GroupCategory.objects.get(key=parameter_group['category'])) \
+                                        .only("key") \
+                                        .values_list("key", flat=True))
+                        else:
+                            groups = parameter_group['groups']
+
+                        for group in groups:
+                            [distribution, created] = models.Distribution.objects.get_or_create(min=min_value, max=max_value, type='Normal', value=0.0)
+                            group_parameter = models.ScenarioParameterGroup.objects.create(distribution=distribution)
+                            
+                            sub_groups = group.split(",")
+                            for sb in sub_groups:
+                                group_parameter.groups.add(list(filter(lambda g: g.key == sb, all_groups))[0])
+
+                            group_parameter.save()
+                            group_parameters.append(group_parameter)
+
+                    progress.update()
 
                     scenario_parameter.groups.set(group_parameters)
                     scenario_parameter.save()
