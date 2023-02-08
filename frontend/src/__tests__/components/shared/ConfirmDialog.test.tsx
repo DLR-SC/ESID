61a4ed05d64c9bd3057e0eac8ee5b172b8d21eb2
Jonas Gilg <jonas.gilg@dlr.de>
2023-02-08 13:38:25 +0100

    :heavy_check_mark: Add a test case for the ConfirmDialog.

---
+++ frontend/src/__tests__/components/shared/ConfirmDialog.test.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import { render, fireEvent, screen } from '@testing-library/react';
+import ConfirmDialog from "../../../components/shared/ConfirmDialog";
+
+describe('ConfirmDialog', () => {
+  it('renders the correct title and text', () => {
+    const handleAnswer = jest.fn();
+
+    const title = 'Confirm Action';
+    const text = 'Are you sure you want to perform this action?';
+
+    render(
+      <ConfirmDialog open title={title} text={text} onAnswer={handleAnswer} />
+    );
+
+    screen.getByText(title);
+    screen.getByText(text);
+  });
+
+  it('calls the onAnswer prop with the correct answer', () => {
+    const handleAnswer = jest.fn();
+
+    render(
+      <ConfirmDialog open title='Confirm Action' text='Are you sure?' onAnswer={handleAnswer} />
+    );
+
+    const abortButton = screen.getByText('Abort');
+    fireEvent.click(abortButton);
+    expect(handleAnswer).toHaveBeenCalledWith(false);
+
+    const confirmButton = screen.getByText('Confirm');
+    fireEvent.click(confirmButton);
+    expect(handleAnswer).toHaveBeenCalledWith(true);
+  });
+
+  it('renders the correct button text', () => {
+    const handleAnswer = jest.fn();
+    const abortButtonText = 'Cancel';
+    const confirmButtonText = 'Yes, I am sure';
+
+    render(
+      <ConfirmDialog
+        open
+        title='Confirm Action'
+        text='Are you sure?'
+        onAnswer={handleAnswer}
+        abortButtonText={abortButtonText}
+        confirmButtonText={confirmButtonText}
+      />
+    );
+
+    screen.getByText(abortButtonText);
+    screen.getByText(confirmButtonText);
+  });
+});
