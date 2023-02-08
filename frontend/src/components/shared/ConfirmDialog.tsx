9aeaa92f946002249df40ead5e3c2a78db0a7ff4
Jonas Gilg <jonas.gilg@dlr.de>
2023-02-08 11:24:13 +0100

    :wrench: Optimize imports.

--- frontend/src/components/shared/ConfirmDialog.tsx
+++ frontend/src/components/shared/ConfirmDialog.tsx
@@ -1,6 +1,10 @@
-import React from 'react';
-import {Box, Button, Dialog, Divider, Typography} from '@mui/material';
-import {useTheme} from '@mui/material/styles';
+import React from "react";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import Divider from "@mui/material/Divider";
+import Typography from "@mui/material/Typography";
+import { useTheme } from "@mui/material/styles";
 
 export interface ConfirmDialogProps {
   open: boolean;
@@ -16,36 +20,36 @@ export default function ConfirmDialog(props: ConfirmDialogProps): JSX.Element {
   const theme = useTheme();
 
   return (
-    <Dialog open={props.open} sx={{backdropFilter: 'none'}}>
+    <Dialog open={props.open} sx={{ backdropFilter: "none" }}>
       <Box
         sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          padding: theme.spacing(4),
+          display: "flex",
+          flexDirection: "column",
+          padding: theme.spacing(4)
         }}
       >
-        <Typography variant='h2'>{props.title}</Typography>
-        <Divider orientation='horizontal' flexItem />
-        <Typography variant='body1' sx={{paddingY: theme.spacing(3)}}>
+        <Typography variant="h2">{props.title}</Typography>
+        <Divider orientation="horizontal" flexItem />
+        <Typography variant="body1" sx={{ paddingY: theme.spacing(3) }}>
           {props.text}
         </Typography>
         <Box
           sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'flex-end',
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "flex-end"
           }}
         >
           <Button
-            variant='outlined'
-            color='error'
-            sx={{marginRight: theme.spacing(2)}}
+            variant="outlined"
+            color="error"
+            sx={{ marginRight: theme.spacing(2) }}
             onClick={() => props.onAnswer(false)}
           >
-            {props.abortButtonText || 'Abort'}
+            {props.abortButtonText || "Abort"}
           </Button>
-          <Button variant='outlined' color='primary' onClick={() => props.onAnswer(true)}>
-            {props.confirmButtonText || 'Confirm'}
+          <Button variant="outlined" color="primary" onClick={() => props.onAnswer(true)}>
+            {props.confirmButtonText || "Confirm"}
           </Button>
         </Box>
       </Box>
