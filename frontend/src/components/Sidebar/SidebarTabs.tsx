84bd93883665c8e74087576ef8abdb830d1ab8c3
Jonas Gilg <jonas.gilg@dlr.de>
2023-08-22 09:21:29 +0200

    :sparkles: Run formatter.

--- frontend/src/components/Sidebar/SidebarTabs.tsx
+++ frontend/src/components/Sidebar/SidebarTabs.tsx
@@ -18,10 +18,12 @@ export default function SidebarTabs(): JSX.Element {
       }}
     >
       {/* This is a placeholder for now. */}
-      <a href={`https://www.helmholtz.de/loki-pandemics/${i18n.language.includes('de') ? '' : 'en/'}`}
-         target='_blank'
-         rel='noopener noreferrer'
-         style={{width: '40%'}}>
+      <a
+        href={`https://www.helmholtz.de/loki-pandemics/${i18n.language.includes('de') ? '' : 'en/'}`}
+        target='_blank'
+        rel='noopener noreferrer'
+        style={{width: '40%'}}
+      >
         <img src={logo} alt={t('loki-logo')} width='100%' />
       </a>
     </Box>
