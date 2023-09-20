0107f90e117a970e59c77a3827b12ab7d424b0dc
Jonas Gilg <jonas.gilg@dlr.de>
2023-09-20 13:18:35 +0200

    :wrench: Rename simulation start day to reference day.

--- frontend/src/components/Scenario/CompartmentList.tsx
+++ frontend/src/components/Scenario/CompartmentList.tsx
@@ -148,7 +148,7 @@ function SimulationStartTitle(): JSX.Element {
           fontSize: '13pt',
         }}
       >
-        {t('scenario.simulation-start-day')}:
+        {t('scenario.reference-day')}:
       </Typography>
       <Typography
         variant='h2'
