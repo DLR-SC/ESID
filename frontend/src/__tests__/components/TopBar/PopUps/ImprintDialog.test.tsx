7ccbbfc66841e5e8518d68d16ba926743f1b8703
JonasGilg <jonas.gilg@dlr.de>
2024-02-22 11:40:15 +0100

    :beetle: Fix tests.

--- frontend/src/__tests__/components/TopBar/PopUps/ImprintDialog.test.tsx
+++ frontend/src/__tests__/components/TopBar/PopUps/ImprintDialog.test.tsx
@@ -1,7 +1,7 @@
 // SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
 // SPDX-License-Identifier: Apache-2.0
 
-import React from 'react';
+import React, {Suspense} from 'react';
 import {describe, test} from 'vitest';
 import {render, screen} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -15,7 +15,9 @@ describe('ImprintDialog', () => {
   test('PopUp', async () => {
     render(
       <I18nextProvider i18n={i18n}>
-        <ApplicationMenu />
+        <Suspense>
+          <ApplicationMenu />
+        </Suspense>
       </I18nextProvider>
     );
     const menu = screen.getByLabelText('topBar.menu.label');
@@ -24,7 +26,7 @@ describe('ImprintDialog', () => {
     const imprint = screen.getByText('topBar.menu.imprint');
     await userEvent.click(imprint);
 
-    screen.getByText('imprint.header');
-    screen.getByText('imprint.content');
+    await screen.findByText('imprint.header');
+    await screen.findByText('imprint.content');
   });
 });
