4f751a9962e4974dd10a9770c49f719a80fe4195
Jonas Gilg <jonas.gilg@dlr.de>
2024-03-21 16:31:37 +0100

    :memo: Add some documentation.

--- frontend/src/components/shared/MathMarkdown.tsx
+++ frontend/src/components/shared/MathMarkdown.tsx
@@ -6,6 +6,9 @@ import ReactMarkdown from 'react-markdown';
 import rehypeKatex from 'rehype-katex';
 import remarkMath from 'remark-math';
 
+/**
+ * Renders the given content in a markdown environment that supports mathematical equations.
+ */
 export default function MathMarkdown(props: Readonly<{children: string}>): JSX.Element {
   return (
     <ReactMarkdown remarkPlugins={[remarkMath]} rehypePlugins={[rehypeKatex]}>
