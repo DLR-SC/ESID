4f86bda14ac989f6b73f7488c131e90c8e10e6d6
Jackie <kanakanajm@outlook.com>
2024-06-14 11:12:05 +0200

    :wrench: Fix provider and store missing test errors in TopBar related components

--- frontend/src/components/AuthProvider.tsx
+++ frontend/src/components/AuthProvider.tsx
@@ -12,15 +12,31 @@ interface AuthProviderProps {
 
 function AuthProvider({children}: AuthProviderProps) {
   const realm = useAppSelector((state) => state.realm.name);
+  let authConfig: TAuthConfig;
 
-  const authConfig: TAuthConfig = {
-    clientId: `${import.meta.env.VITE_OAUTH_CLIENT_ID || ''}`,
-    authorizationEndpoint: `${import.meta.env.VITE_OAUTH_API_URL || ''}/realms/${realm}/protocol/openid-connect/auth`,
-    tokenEndpoint: `${import.meta.env.VITE_OAUTH_API_URL || ''}/realms/${realm}/protocol/openid-connect/token`,
-    redirectUri: window.location.origin, // always redirect to root
-    scope: 'openid profile email', // default scope without audience
-    autoLogin: false,
-  };
+  if (!import.meta.env.VITE_OAUTH_CLIENT_ID || !import.meta.env.VITE_OAUTH_API_URL) {
+    // in case required auth env vars are not set
+    console.warn(
+      'Missing environment variables: VITE_OAUTH_CLIENT_ID or VITE_OAUTH_API_URL. Please set it to enable authentication.'
+    );
+    authConfig = {
+      clientId: 'client-placeholder',
+      authorizationEndpoint: 'auth-endpoint-placeholder',
+      tokenEndpoint: 'token-endpoint-placeholder',
+      redirectUri: window.location.origin,
+      autoLogin: false,
+    };
+  } else {
+    // actual auth configurations
+    authConfig = {
+      clientId: `${import.meta.env.VITE_OAUTH_CLIENT_ID}`,
+      authorizationEndpoint: `${import.meta.env.VITE_OAUTH_API_URL}/realms/${realm}/protocol/openid-connect/auth`,
+      tokenEndpoint: `${import.meta.env.VITE_OAUTH_API_URL}/realms/${realm}/protocol/openid-connect/token`,
+      redirectUri: window.location.origin, // always redirect to root
+      scope: 'openid profile email', // default scope without audience
+      autoLogin: false,
+    };
+  }
 
   return <OAuth2WithPkceProvider authConfig={authConfig}>{children}</OAuth2WithPkceProvider>;
 }
