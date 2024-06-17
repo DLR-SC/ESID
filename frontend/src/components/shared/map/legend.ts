e1aa36c7cf9eb5c7ac4fdebc0b8d47a4abe7d104
Violini <giacomo.violini@dlr.de>
2024-06-17 14:39:40 +0200

    :sparkles: Add Reuse Compliance

--- frontend/src/components/shared/map/legend.ts
+++ frontend/src/components/shared/map/legend.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+// SPDX-License-Identifier: Apache-2.0
+
 import {useState, useLayoutEffect} from 'react';
 import {HeatLegend, IHeatLegendSettings} from '@amcharts/amcharts5';
 import * as am5 from '@amcharts/amcharts5';
