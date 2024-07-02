a0ce63f18347181f32d11006a8905614a58485b4
Violini <giacomo.violini@dlr.de>
2024-07-02 14:50:31 +0200

    :sparkles: Add reuse compliance

--- frontend/src/types/lineChart.ts
+++ frontend/src/types/lineChart.ts
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+// SPDX-License-Identifier: Apache-2.0
+
 import {Color} from '@amcharts/amcharts5/.internal/core/util/Color';
 
 export interface LineChartData {
