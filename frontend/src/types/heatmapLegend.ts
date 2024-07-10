a5e036db84efe9bdee66a5db5b4df5afe52688af
Serloni <lorenzo.serloni@dlr.de>
2024-07-10 10:30:17 +0200

    :sparkle: Fix the formatting

--- frontend/src/types/heatmapLegend.ts
+++ frontend/src/types/heatmapLegend.ts
@@ -9,12 +9,12 @@ export interface HeatmapLegend {
    * The name of the legend.
    */
   name: string;
-  
+
   /**
    * Indicates whether the legend values are normalized.
    */
   isNormalized: boolean;
-  
+
   /**
    * The steps in the legend, each containing a color and a corresponding value.
    */
@@ -23,7 +23,7 @@ export interface HeatmapLegend {
      * The color associated with the value.
      */
     color: string;
-    
+
     /**
      * The value associated with the color.
      */
