5f2c8891d3fb6e57d5799e1ceb79b8c757555c12
emilijadunoska <emilijadunoska@gmail.com>
2024-08-22 10:09:09 +0200

    🔧 Fix height of top bar popover

--- frontend/src/components/OnboardingComponents/TopBarPopover.tsx
+++ frontend/src/components/OnboardingComponents/TopBarPopover.tsx
@@ -47,7 +47,6 @@ export default function TopBarPopover(props: TopBarPopoverProps): JSX.Element {
       sx={{
         '& .MuiPopover-paper': {
           width: 600,
-          height: 350,
         },
       }}
     >
