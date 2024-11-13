467b8bfd25e998b3c26e1ae92fb11b3eb325d373
soybrian <brian.beckmann@dlr.de>
2024-11-13 13:22:52 +0100

    refactor(searchbar): changed searchbox style

--- frontend/src/components/Sidebar/MapComponents/SearchBar.tsx
+++ frontend/src/components/Sidebar/MapComponents/SearchBar.tsx
@@ -76,50 +76,52 @@ export default function SearchBar({
           alignItems: 'center',
           alignContent: 'center',
           width: 1,
-          my: 3, // sx-shorthand for margin-top & -bottom = theme.spacing(3)
-          borderRadius: 4,
-          background: theme.palette.background.default,
+          my: 3,
+          borderRadius: 1,
+          background: 'white',
           borderStyle: 'solid',
-          borderWidth: '2px',
+          borderWidth: '1px',
           borderColor: theme.palette.divider,
-          '&:hover': {borderColor: theme.palette.primary.light},
-          '&:hover *': {borderColor: theme.palette.primary.light},
-          '&:focus-within': {borderColor: theme.palette.primary.main},
-          '&:focus-within *': {borderColor: theme.palette.primary.main},
+          transition: 'border-color 0.3s ease-in-out',
+          '&:hover': {
+            borderColor: 'rgba(0,0,0,.55)',
+          },
+          '&:hover *': {
+            borderColor: theme.palette.primary.light,
+            transition: 'border-color 0.3s ease-in-out',
+          },
+          '&:focus-within': {
+            borderColor: theme.palette.primary.main,
+          },
+          '&:focus-within *': {
+            borderColor: theme.palette.primary.main,
+          },
         }}
       >
         <SearchIcon
-          color='primary'
           sx={{
-            pl: 2, // sx-shorthand for padding-left = theme.spacing(2)
-            pr: 1, // sx-shorthand for padding-right = theme.spacing(1)
+            pl: 2,
+            pr: 0.5,
+            py: 0.5,
+            borderRadius: '3px',
+            color: 'rgba(0,0,0, 0.3)',
+            width: '18px',
+            background: 'white',
           }}
         />
         <Autocomplete
-          // set value to selectedDistrict contents from store
           value={autoCompleteValue}
-          // function to compare value to options in list, comparing ags number and name of district
           isOptionEqualToValue={(option, value) => option![optionEqualProperty] === value![valueEqualProperty]}
-          // onChange of input dispatch new selected district or initial value (ags: 00000, name: germany) if input is cleared
           onChange={onChange}
-          // enable clearing/resetting the input field with escape key
           clearOnEscape
-          // automatically highlights first option
           autoHighlight
-          // selects highlighted option on focus loss
-          //autoSelect
-          // provide countyList as options for drop down
           options={data || []}
-          // group dropdown contents by first letter (json array needs to be sorted alphabetically by name for this to work correctly)
           groupBy={sortProperty ? (option) => (option![sortProperty] as string)[0] : undefined}
-          // provide function to display options in dropdown menu
           getOptionLabel={optionLabel}
           sx={{
             flexGrow: 1,
-            //disable outline for any children
             '& *:focus': {outline: 'none'},
           }}
-          // override default input field, placeholder is a fallback, as value should always be a selected district or germany (initial/default value)
           renderInput={(params) => (
             <div
               ref={params.InputProps.ref}
@@ -133,10 +135,12 @@ export default function SearchBar({
                 style={{
                   flexGrow: 1,
                   borderStyle: 'none',
-                  fontSize: '16px',
+                  fontSize: '14px',
                   padding: '5px',
-                  borderTopRightRadius: 26,
-                  borderBottomRightRadius: 26,
+                  background: 'white',
+                  borderRadius: '3px',
+                  fontWeight: 'bold',
+                  color: 'rgba(0, 0, 0, 0.8)',
                 }}
                 placeholder={placeholder}
               />
