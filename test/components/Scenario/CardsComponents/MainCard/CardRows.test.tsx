b7be64ee7a7cdc7e4d11868425c578d285163efd
Jonas Gilg <jonas.gilg@dlr.de>
2025-05-27 18:51:47 +0200

    :wrench: Apply suggestion.
    Co-authored-by: Moritz Zeumer <25636783+NXXR@users.noreply.github.com>

--- test/components/Scenario/CardsComponents/MainCard/CardRows.test.tsx
+++ test/components/Scenario/CardsComponents/MainCard/CardRows.test.tsx
@@ -72,7 +72,6 @@ describe('CardRows Component', () => {
     render(
       <ThemeProvider theme={Theme}>
         <CardRows
-          index={0}
           compartments={compartments}
           compartmentValues={compartmentValues}
           referenceValues={startValues}
