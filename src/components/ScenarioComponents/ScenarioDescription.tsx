5e0200b585077472a2e1f561bb75d51acf09e7ab
Jonas Gilg <jonas.gilg@dlr.de>
2025-07-10 14:01:03 +0200

    :memo: Add spdx header to ScenarioDescription.tsx.

--- src/components/ScenarioComponents/ScenarioDescription.tsx
+++ src/components/ScenarioComponents/ScenarioDescription.tsx
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2024 German Aerospace Center (DLR)
+// SPDX-License-Identifier: Apache-2.0
+
 import React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
