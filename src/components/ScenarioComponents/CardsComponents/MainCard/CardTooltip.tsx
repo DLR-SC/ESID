53e215b1537bd8f66dc03c4b6c65fcbedb0f3098
Jonas Gilg <jonas.gilg@dlr.de>
2025-07-14 09:08:21 +0200

    :sparkles: Formatting.

--- src/components/ScenarioComponents/CardsComponents/MainCard/CardTooltip.tsx
+++ src/components/ScenarioComponents/CardsComponents/MainCard/CardTooltip.tsx
@@ -168,7 +168,9 @@ export default function CardTooltip({
                 event.stopPropagation();
                 setShowDescription(!showDescription);
               }}
-              aria-label={showDescription ? defaultT('scenario.hide-description') : defaultT('scenario.show-description')}
+              aria-label={
+                showDescription ? defaultT('scenario.hide-description') : defaultT('scenario.show-description')
+              }
             >
               {showDescription ? <TableRowsIcon /> : <InfoIcon />}
             </IconButton>
