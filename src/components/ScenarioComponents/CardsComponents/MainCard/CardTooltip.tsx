26d7adc7b9120596799d13a1278798debc5f8d45
Jonas Gilg <jonas.gilg@dlr.de>
2025-07-14 09:04:53 +0200

    :wrench: Apply suggestion.
    Co-authored-by: Moritz Zeumer <25636783+NXXR@users.noreply.github.com>

--- src/components/ScenarioComponents/CardsComponents/MainCard/CardTooltip.tsx
+++ src/components/ScenarioComponents/CardsComponents/MainCard/CardTooltip.tsx
@@ -159,7 +159,7 @@ export default function CardTooltip({
         </Tooltip>
         {isActive && (
           <Tooltip
-            title={showDescription ? defaultT('scenario.hideDescription') : defaultT('scenario.showDescription')}
+            title={showDescription ? defaultT('scenario.hide-description') : defaultT('scenario.show-description')}
             arrow={true}
           >
             <IconButton
